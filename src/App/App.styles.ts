c1c34a40dd48a451075875cf816916b56aac1a12
Shawn Hoerner <shoerner+git@shoerner.net>
2018-09-19 23:07:07 -0400

    Create basic app using TypeScript CRA

---
+++ src/App/App.styles.ts
@@ -0,0 +1,30 @@
+import { StyleSheet } from 'aphrodite';
+
+const logoSpinKeyframes = {
+  'from': { transform: 'rotate(0deg)' },
+  'to': { transform: 'rotate(360deg)' }
+}
+
+export const styles = StyleSheet.create({
+  app: {
+    textAlign: 'center',
+  },
+  appHeader: {
+    backgroundColor: '#222',
+    color: 'white',
+    height: '150px',
+    padding: '20px',
+  },
+  appIntro: {
+    fontSize: 'large',
+  },
+  appLogo: {
+    animationDuration: '20s',
+    animationIterationCount: 'infinite',
+    animationName: [logoSpinKeyframes],
+    height: '80px',
+  },
+  appTitle: {
+    fontSize: '1.5em',
+  },
+})
